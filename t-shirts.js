2584a035c9b525c46430b78ccee6ba81dfd63829
liuniuzhang <zhan0884@algonquinlive.com>
2025-06-18 18:51:40 -0400

    complete assignment

--- t-shirts.js
+++ t-shirts.js
@@ -1,13 +1,13 @@
 const tshirtsData = [
-  { title: 'Blue T-Shirt', image: 'images/blue-t-shirt.jpg', price: 7.99, stock: 4, quantity: 1 },
+  { title: 'Blue T-Shirt', image: 'images/blue-t-shirt.jpg', price: 7.99, stock: 40, quantity: 1 },
   { title: 'Bright Purple T-Shirt', image: 'images/bright-purple-t-shirt.jpg', price: 5.99, stock: 1, quantity: 1 },
-  { title: 'Cobalt Blue T-Shirt', image: 'images/cobalt-blue-t-shirt.jpg', price: 9.99, stock: 5, quantity: 1 },
+  { title: 'Cobalt Blue T-Shirt', image: 'images/cobalt-blue-t-shirt.jpg', price: 9.99, stock: 50, quantity: 1 },
   { title: 'Green T-Shirt', image: 'images/green-t-shirt.jpg', price: 6.99, stock: 0, quantity: 1 },
-  { title: 'Grey T-Shirt', image: 'images/blue-t-shirt.jpg', price: 4.99, stock: 2, quantity: 1 },
-  { title: 'Light Green T-Shirt', image: 'images/light-green-t-shirt.jpg', price: 7.99, stock: 4, quantity: 1 },
-  { title: 'Purple T-Shirt', image: 'images/purple-t-shirt.jpg', price: 7.99, stock: 0, quantity: 1 },
+  { title: 'Grey T-Shirt', image: 'images/blue-t-shirt.jpg', price: 4.99, stock: 22, quantity: 1 },
+  { title: 'Light Green T-Shirt', image: 'images/light-green-t-shirt.jpg', price: 7.99, stock: 0, quantity: 1 },
+  { title: 'Purple T-Shirt', image: 'images/purple-t-shirt.jpg', price: 7.99, stock: 30, quantity: 1 },
   { title: 'Red T-Shirt', image: 'images/red-t-shirt.jpg', price: 6.99, stock: 3, quantity: 1 },
-  { title: 'Teal T-Shirt', image: 'images/teal-t-shirt.jpg', price: 7.99, stock: 2, quantity: 1 }
+  { title: 'Teal T-Shirt', image: 'images/teal-t-shirt.jpg', price: 7.99, stock: 0, quantity: 1 }
 ];
 
 function TShirt({ tshirt, onBuy, onQuantityChange }) {
